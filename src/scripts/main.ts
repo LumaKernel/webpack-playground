0485632322f2d11c93cf129f4a1455813135848d
Luma <markermails@gmail.com>
2020-06-06 01:09:56 +0900

    play/anime.js

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,3 +1,18 @@
-const div = document.createElement('h3');
-div.innerText = 'Hello from TypeScript';
-document.body.appendChild(div);
+import anime from "animejs";
+
+anime({
+  targets: 'div',
+  translateX: 450,
+  translateY: -100,
+  rotate: '1turn',
+  backgroundColor: '#913',
+  color: "#f3f3f3",
+  duration: 1500,
+  scale: () => 2.5,
+  loop: true,
+  direction: 'alternate',
+  borderRadius: () => '8%',
+  roteate: () => anime.random(-360, 360),
+  easing: 'easeOutElastic(1, 0.5)',
+});
+
